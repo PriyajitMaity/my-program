ce40a8391c8158bf93e2d666f795da6558302065
PriyajitMaity <priyajitmaity023@gmail.com>
2025-08-30 17:30:19 +0530

    initial

--- JavaScript/programs/duplicate.js
+++ JavaScript/programs/duplicate.js
@@ -2,5 +2,5 @@ const nums =[1, 2, 2, 3, 4, 1];
 console.log(findDuplicate(nums));
 
 function findDuplicate(a) {
-    return a.filter((i, index) =>a.indexOf(i) === a.lastIndexOf(i));
+    return a.filter((i, index) =>a.indexOf(i) !== index);
 }
